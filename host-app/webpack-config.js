c152a6b7dc7fdff37cdb2a2e1113585c9b70a7dd
Dave Baskin <dfbaskin@gmail.com>
2022-07-29 11:54:36 -0400

    Cleanup build.

--- host-app/webpack-config.js
+++ host-app/webpack-config.js
@@ -1,18 +1,22 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin =
   require("webpack").container.ModuleFederationPlugin;
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
+
+const outputPath = path.join(__dirname, "../dist/host-app");
 
 module.exports = {
   entry: "./host-app/src/main",
   mode: "development",
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: outputPath
     },
     port: 4200,
   },
   output: {
+    path: outputPath,
     publicPath: "auto",
   },
   resolve: {
@@ -44,6 +48,9 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: "./host-app/src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./host-app/src/favicon.ico", to: outputPath }],
     }),
   ],
 };
