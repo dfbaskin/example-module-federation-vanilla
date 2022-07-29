e69e328d60f60226d82bd534da1f9167db3f203a
Dave Baskin <dfbaskin@gmail.com>
2022-07-29 15:37:14 -0400

    Updating styles.

--- interop-app/webpack-config.js
+++ interop-app/webpack-config.js
@@ -28,7 +28,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["css-loader"],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.tsx?$/,
